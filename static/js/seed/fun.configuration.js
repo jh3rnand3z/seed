d4534cb026b6e423b23ae4d941c5cbb4ca61ce85
Jean Chassoul <chassoul@gmail.com>
2014-09-08 20:49:21 -0600

    string format

--- static/js/seed/fun.configuration.js
+++ static/js/seed/fun.configuration.js
@@ -5,7 +5,7 @@ fun.conf = {
     account: 'account',
     urlRoot: '/api/',
 
-    html: '/static/html/',
+    html: '/static/html',
     domain: 'iofun.io',
 
     uuidRecord: 'record_uuid',
@@ -96,26 +96,26 @@ fun.conf.urls = {
     user: fun.utils.format('/users/%s', fun.conf.account),
     users: '/users',
 
-    org: '/orgs/' + fun.conf.account,
+    org: fun.utils.format('/orgs/%s', fun.conf.account),
     orgs: '/orgs/',
 
-    record: '/records/' + fun.conf.uuidRecord,
+    record: fun.utils.format('/records/%s', fun.conf.uuidRecord),
     records: '/records',
 
-    billing: '/billings/' + fun.conf.uuidBilling,
+    billing: fun.utils.format('/billings/%s', fun.conf.uuidBilling),
     billings: '/billings',
     
     summary: '/records/summary',
     summaries: '/records/summaries',
 
-    summaryStart: '/records/summary/start/' + fun.conf.startTime,
-    summaryStartEnd: '/records/summary/start/' + fun.conf.startTime + '/end/' + fun.conf.endTime,
+    summaryStart: fun.utils.format('/records/summary/start/%s', fun.conf.startTime),
+    summaryStartEnd: fun.utils.format('/records/summary/start/%s/end/%s', fun.conf.startTime, fun.conf.endTime),
 
-    summariesStart: '/records/summaries/start/' + fun.conf.startTime,
+    summariesStart: fun.utils.format('/records/summaries/start/%s', fun.conf.startTime),
     summariesStartEnd: '/records/summaries/start/' + fun.conf.startTime + '/end/' + fun.conf.endTime,
 
-    lapseSummary: '/records/summary/' + fun.conf.lapse,
-    lapseSummaries: '/records/summaries/' + fun.conf.lapse,
+    lapseSummary: fun.utils.format('/records/summary/%s', fun.conf.lapse),
+    lapseSummaries: fun.utils.format('/records/summaries/%s', fun.conf.lapse),
 
     lapseSummaryStart: '/records/summary/' + fun.conf.lapse + '/start/' + fun.conf.startTime,
     lapseSummaryStartEnd: '/records/summary/' + fun.conf.lapse + '/start/' + fun.conf.startTime + '/end/' + fun.conf.endTime,
@@ -124,16 +124,16 @@ fun.conf.urls = {
     lapseSummariesStartEnd: '/records/summaries/' + fun.conf.lapse + '/start/' + fun.conf.startTime + '/end/' + fun.conf.endTime,
 
 
-    recordsStart: '/records/start/' + fun.conf.startTime,
+    recordsStart: fun.utils.format('/records/start/%s', fun.conf.startTime),
     recordsStartEnd: '/records/start/' + fun.conf.startTime + '/end/' + fun.conf.endTime,
 
-    billingsRecord: '/billings/records/' + fun.conf.uuidRecord,
+    billingsRecord: fun.utils.format('/billings/records/%s', fun.conf.uuidRecord),
     billingsRecords: '/billings/records/',
     
-    billingsStart: '/billings/start/' + fun.conf.startTime,
+    billingsStart: fun.utils.format('/billings/start/%s', fun.conf.startTime),
     billingsStartEnd: '/billings/start/' + fun.conf.startTime + '/end/' + fun.conf.endTime,
 
-    billingsRecordsStart: '/billings/records/start/' + fun.conf.startTime,
+    billingsRecordsStart: fun.utils.format('/billings/records/start/%s', fun.conf.startTime),
     billingsRecordsStartEnd: '/billings/records/start/' + fun.conf.startTime + '/end/' + fun.conf.endTime,
 
 
@@ -147,101 +147,101 @@ fun.conf.urls = {
 fun.conf.templates = {
     navbar: fun.utils.format('%s/navbar.html', fun.conf.html),
     
-    navLanding: fun.conf.html + '/navLanding.html',
-    navDashboard: fun.conf.html + '/navDashboard.html',
+    navLanding: fun.utils.format('%s/navLanding.html', fun.conf.html),
+    navDashboard: fun.utils.format('%s/navDashboard.html', fun.conf.html),
     
-    navAdmin: fun.conf.html + '/navAdmin.html', // ultimate junk!
+    navAdmin: fun.utils.format('%s/navAdmin.html', fun.conf.html), // ultimate junk!
 
-    subheader: fun.conf.html + '/subheader.html',
-    headNav: fun.conf.html + '/headNav.html',
-    headNavCampaigns: fun.conf.html + '/headNavCampaigns.html',
-    headNavReports: fun.conf.html + '/headNavReports.html',
+    subheader: fun.utils.format('%s/subheader.html', fun.conf.html),
+    headNav: fun.utils.format('%s/headNav.html', fun.conf.html),
+    headNavCampaigns: fun.utils.format('%s/headNavCampaigns.html', fun.conf.html),
+    headNavReports: fun.utils.format('%s/headNavReports.html', fun.conf.html),
     
-    landing: fun.conf.html + '/landing.html',
+    landing: fun.utils.format('%s/landing.html', fun.conf.html),
     
-    support: fun.conf.html + '/support.html',
-    contact: fun.conf.html + '/contact.html',
-    features: fun.conf.html + '/features.html',
-    enterprise: fun.conf.html + '/enterprise.html',
-    howto: fun.conf.html + '/howto.html',
-    status: fun.conf.html + '/status.html',
-    developers: fun.conf.html + '/developers.html',
-    help: fun.conf.html + '/help.html',
-    security: fun.conf.html + '/security.html',
-    terms: fun.conf.html + '/terms.html',
+    support: fun.utils.format('%s/support.html', fun.conf.html),
+    contact: fun.utils.format('%s/contact.html', fun.conf.html),
+    features: fun.utils.format('%s/features.html', fun.conf.html),
+    enterprise: fun.utils.format('%s/enterprise.html', fun.conf.html),
+    howto: fun.utils.format('%s/howto.html', fun.conf.html),
+    status: fun.utils.format('%s/status.html', fun.conf.html),
+    developers: fun.utils.format('%s/developers.html', fun.conf.html),
+    help: fun.utils.format('%s/help.html', fun.conf.html),
+    security: fun.utils.format('%s/security.html', fun.conf.html),
+    terms: fun.utils.format('%s/terms.html', fun.conf.html),
 
-    privacy: fun.conf.html + '/privacy.html',
+    privacy: fun.utils.format('%s/privacy.html', fun.conf.html),
 
     
-    signup: fun.conf.html + '/signup.html',
+    signup: fun.utils.format('%s/signup.html', fun.conf.html),
 
-    login: fun.conf.html + '/login.html',
+    login: fun.utils.format('%s/login.html', fun.conf.html),
     
-    dashboard: fun.conf.html + '/dashboard.html',
+    dashboard: fun.utils.format('%s/dashboard.html', fun.conf.html),
 
-    orgs: fun.conf.html + '/orgs.html',
-    campaigns: fun.conf.html + '/campaigns.html',
+    orgs: fun.utils.format('%s/orgs.html', fun.conf.html),
+    campaigns: fun.utils.format('%s/campaigns.html', fun.conf.html),
     
-    recordRow: fun.conf.html + '/recordRow.html',
-    typeRow: fun.conf.html + '/typeRow.html',
-    sumRow: fun.conf.html + '/sumRow.html',
+    recordRow: fun.utils.format('%s/recordRow.html', fun.conf.html),
+    typeRow: fun.utils.format('%s/typeRow.html', fun.conf.html),
+    sumRow: fun.utils.format('%s/sumRow.html', fun.conf.html),
 
-    lastRecord: fun.conf.html + '/lastRecord.html',
-    latestRecords: fun.conf.html + '/latestRecords.html',
+    lastRecord: fun.utils.format('%s/lastRecord.html', fun.conf.html),
+    latestRecords: fun.utils.format('%s/latestRecords.html', fun.conf.html),
 
-    recordType: fun.conf.html + '/recordType.html',  
-    recordSummary: fun.conf.html + '/recordSummary.html',
+    recordType: fun.utils.format('%s/recordType.html', fun.conf.html),
+    recordSummary: fun.utils.format('%s/recordSummary.html', fun.conf.html),
 
-    todaySummary: fun.conf.html + '/todaySummary.html',
-    todayActivityChart: fun.conf.html + '/todayActivityChart.html',
+    todaySummary: fun.utils.format('%s/todaySummary.html', fun.conf.html),
+    todayActivityChart: fun.utils.format('%s/todayActivityChart.html', fun.conf.html),
 
-    dialpad: fun.conf.html + '/dialpad.html',
+    dialpad: fun.utils.format('%s/dialpad.html', fun.conf.html),
     
-    messages: fun.conf.html + '/messages.html',
-    videos: fun.conf.html + '/videos.html',
+    messages: fun.utils.format('%s/messages.html', fun.conf.html),
+    videos: fun.utils.format('%s/videos.html', fun.conf.html),
 
-    controlTo: fun.conf.html + '/controlTo.html',
-    controlFrom: fun.conf.html + '/controlFrom.html',
-    findLapse: fun.conf.html + '/findLapse.html',
+    controlTo: fun.utils.format('%s/controlTo.html', fun.conf.html),
+    controlFrom: fun.utils.format('%s/controlFrom.html', fun.conf.html),
+    findLapse: fun.utils.format('%s/findLapse.html', fun.conf.html),
     
-    message: fun.conf.html + '/message.html',
-    messageSmall: fun.conf.html + '/messageSmall.html',
-    messageMedium: fun.conf.html + '/messageMedium',
-    messageLarge: fun.conf.html + '/messageLarge',
+    message: fun.utils.format('%s/message.html', fun.conf.html),
+    messageSmall: fun.utils.format('%s/messageSmall.html', fun.conf.html),
+    messageMedium: fun.utils.format('%s/messageMedium', fun.conf.html),
+    messageLarge: fun.utils.format('%s/messageLarge', fun.conf.html),
 
-    warning: fun.conf.html + '/warning.html',
-    warningSmall: fun.conf.html + '/warningSmall.html',
-    warningMedium: fun.conf.html + '/warningMedium.html',
-    warningLarge: fun.conf.html + '/warningLarge.html',
+    warning: fun.utils.format('%s/warning.html', fun.conf.html),
+    warningSmall: fun.utils.format('%s/warningSmall.html', fun.conf.html),
+    warningMedium: fun.utils.format('%s/warningMedium.html', fun.conf.html),
+    warningLarge: fun.utils.format('%s/warningLarge.html', fun.conf.html),
 
-    error: fun.conf.html + '/error.html',
-    errorSmall: fun.conf.html + '/errorSmall.html',
-    errorMedium: fun.conf.html + '/errorMedium.html',
-    errorLarge: fun.conf.html + '/errorLarge.html',
+    error: fun.utils.format('%s/error.html', fun.conf.html),
+    errorSmall: fun.utils.format('%s/errorSmall.html', fun.conf.html),
+    errorMedium: fun.utils.format('%s/errorMedium.html', fun.conf.html),
+    errorLarge: fun.utils.format('%s/errorLarge.html', fun.conf.html),
 
-    phone: fun.conf.html + '/phone.html',
+    phone: fun.utils.format('%s/phone.html', fun.conf.html),
 
-    profile: fun.conf.html + '/profile.html',
+    profile: fun.utils.format('%s/profile.html', fun.conf.html),
 
-    activity: fun.conf.html + '/activity.html',
-    members: fun.conf.html + '/members.html',
-    teams: fun.conf.html + '/teams.html',
+    activity: fun.utils.format('%s/activity.html', fun.conf.html),
+    members: fun.utils.format('%s/members.html', fun.conf.html),
+    teams: fun.utils.format('%s/teams.html', fun.conf.html),
 
-    contacts: fun.conf.html + '/contacts.html',
-    sounds: fun.conf.html + '/sounds.html',
-    recordings: fun.conf.html + '/recordings.html',
-    reports: fun.conf.html + '/reports.html',
-    settings: fun.conf.html + '/settings.html',
+    contacts: fun.utils.format('%s/contacts.html', fun.conf.html),
+    sounds: fun.utils.format('%s/sounds.html', fun.conf.html),
+    recordings: fun.utils.format('%s/recordings.html', fun.conf.html),
+    reports: fun.utils.format('%s/reports.html', fun.conf.html),
+    settings: fun.utils.format('%s/settings.html', fun.conf.html),
 
-    extra: fun.conf.html + '/extra.html',
-    extraNavbar: fun.conf.html + '/extraNavbar.html',
-    extraNavLanding: fun.conf.html + '/extraNavLanding.html',
-    extraNavDashboard: fun.conf.html + '/extraNavDashboard.html',
+    extra: fun.utils.format('%s/extra.html', fun.conf.html),
+    extraNavbar: fun.utils.format('%s/extraNavbar.html', fun.conf.html),
+    extraNavLanding: fun.utils.format('%s/extraNavLanding.html', fun.conf.html),
+    extraNavDashboard: fun.utils.format('%s/extraNavDashboard.html', fun.conf.html),
     
-    social: fun.conf.html + '/social.html',
-    subscribe: fun.conf.html + '/subscribe.html',
+    social: fun.utils.format('%s/social.html', fun.conf.html),
+    subscribe: fun.utils.format('%s/subscribe.html', fun.conf.html),
 
-    footer: fun.conf.html + '/footer.html'
+    footer: fun.utils.format('%s/footer.html', fun.conf.html)
 };
 
 /*
