8aceca8af0d7957b7eaae09c85a91eb9127cb814
Jean Chassoul <chassoul@gmail.com>
2015-03-10 12:28:25 -0600

    new counter

--- static/js/views/dashboard.js
+++ static/js/views/dashboard.js
@@ -12,14 +12,17 @@ fun.views.dashboard = Backbone.View.extend({
     },
 
     initialize: function(options){
+        // initialize view constructor
+        'use strict';
         fun.containers.dashboard = this.$el;
 
         this.account = localStorage.getItem("username");
     },
 
     render: function(account, summary, billing){
-        //account = localStorage.getItem("username");
-
+        
+        // Render view function
+        'use strict';
         console.log("username = " + this.account)
 
         var template = _.template(
@@ -36,44 +39,56 @@ fun.views.dashboard = Backbone.View.extend({
     },
 
     renderAccountDropdown: function(account){
+        // Render account dropdown
+        //'use strict';
+        //console.log(account);
+        var counter, // i
+            length,
+            orgData,
+            itemData,
+            data,
+            itemTemplate;
 
         // Can I get the list from localStorage?, pretty please.
 
         if (account) {
-            data = account.toJSON();
-            this.orgs = data.orgs;
+            //data = account.toJSON();
+
+            console.log(account);
+
+            this.orgs = account.get("orgs");
         } else {
             this.orgs = 0;
         }
 
         this.accountList = this.$('#account-dropdown ul');
 
-        var i = 0;
-        var length = this.orgs.length;
+        counter = 0;
+        length = this.orgs.length;
         
         if (length > 0){
 
-            // da fuq dude?
-            for ( i; i < length; ++i ) {
+            // i, search _.each function
+            for ( counter; counter < length; ++counter ) {
 
-                var orgData = {
-                    'account': data.account,
-                    'org': this.orgs[i]
+                orgData = {
+                    'account': account.get("account"),
+                    'org': this.orgs[counter] // set, put, post, patch
                 };
 
-                var itemData = _.extend(orgData, {i:i+1});
+                itemData = _.extend(orgData, {counter:counter+1});
 
-                var itemTemplate = _.template(
+                itemTemplate = _.template(
                     fun.utils.getTemplate(fun.conf.templates.accountListItem)
                 )(itemData);
 
                 this.accountList.append(itemTemplate);
             }
         }
-        
     },
 
     renderTodaySummary: function(account, summary, billing){
+        // Render today summary
         'use strict';
         var account,
             data,
@@ -115,12 +130,17 @@ fun.views.dashboard = Backbone.View.extend({
     },
 
     renderTodayActivityChart: function(summary){
+        // Render today activity chart
         // This hole summary can be parsed in a single requests
-        // directly to the flot.js library. example on pag 5 of the mango docs.
-        var data = []
-        var minutes = [];
-        var records = [];
-        
+        // directly to the flot.js library.
+        'use strict';
+        var data = [],
+            seconds = [],
+            minutes = [],
+            records = [],
+            template,
+            todayActivityChart;
+
         // check if response from the server
         if(summary){
             this.summary = summary;
@@ -129,12 +149,21 @@ fun.views.dashboard = Backbone.View.extend({
             this.records = summary.get('records');
         }
         
-        // push the seconds missing
+        // push the seconds
+        for (x in this.seconds){
+            seconds.push([x, this.seconds[x]]);
+        }
+
+        data.push({
+            data: seconds,
+            label: 'Seconds'
+        });
 
         // push the minutes
         for (x in this.minutes){
             minutes.push([x, this.minutes[x]]);
         }
+
         data.push({
             data: minutes,
             label: 'Minutes'
@@ -147,15 +176,15 @@ fun.views.dashboard = Backbone.View.extend({
         data.push({
             data: records,
             label: 'Records',
-            points: { show: false }, 
-            lines: { lineWidth: 2, fill: false }
+            points: {show: false}, 
+            lines: {lineWidth: 2, fill: false}
         });
         
-        var template = _.template(
+        template = _.template(
             fun.utils.getTemplate(fun.conf.templates.todayActivityChart)
         )(data);
 
-        var todayActivityChart = this.$('#fun-today-activity-chart');
+        todayActivityChart = this.$('#fun-today-activity-chart');
         todayActivityChart.html(template);
 
         // clean charts
@@ -163,18 +192,23 @@ fun.views.dashboard = Backbone.View.extend({
     },
 
     renderLatestRecords: function(collection){
+        // Render latesst records
+        
+        'use strict';
+        var template,
+            latestRecords;
+
         if (collection) {
             this.collection = collection;
         } else {
             this.collection = 0;
         }
 
-        var template = _.template(
+        template = _.template(
             fun.utils.getTemplate(fun.conf.templates.latestRecords)
         );
 
-        var latestRecords = this.$('#fun-latest-records');
-
+        latestRecords = this.$('#fun-latest-records');
         latestRecords.html(template);
 
         this.tbody = this.$('#records-list > tbody');
@@ -183,12 +217,18 @@ fun.views.dashboard = Backbone.View.extend({
     },
 
     renderRecordType: function(){
-        //var data = {};
+        // Render records type
+        'use strict';
+        var data,
+            series,
+            i,
+            counter,
+            recordType,
+            template;
 
         // Randomly Generated Data
-
-        var data = [],
-            series = Math.floor(Math.random() * 6) + 3;
+        data = [],
+        series = Math.floor(Math.random() * 6) + 3;
 
         for (var i = 0; i < series; i++) {
             data[i] = {
@@ -196,41 +236,26 @@ fun.views.dashboard = Backbone.View.extend({
                 data: Math.floor(Math.random() * 100) + 1
             }
         }
-
-        // A custom label formatter used by several of the plots
+        /*
+        //A custom label formatter used by several of the plots
         function labelFormatter(label, series) {
-            return "<div style='font-size:8pt; text-align:center; padding:2px; color:white;'>" + label + "<br/>" + Math.round(series.percent) + "%</div>";
+            return "<div 
+                            style='font-size:8pt;
+                            text-align:center;
+                            padding:2px; 
+                            color:white;'>"
+                    + label
+                    + "<br/>"
+                    + Math.round(series.percent)
+                    + "%</div>";
         }
-
-        var template = _.template(
+        */
+        template = _.template(
             fun.utils.getTemplate(fun.conf.templates.recordType)
         )(data);
 
-        var recordType = this.$('#fun-record-type');
-        
+        recordType = this.$('#fun-record-type');
         recordType.html(template);
-
-        /*
-        $.plot('#pie-chart', data, {
-            series: {
-                pie: {
-                    show: true,
-                    radius: 1,
-                    label: {
-                        show: true,
-                        radius: 1,
-                        formatter: labelFormatter,
-                        background: {
-                            opacity: 0.8
-                        }
-                    }
-                }
-            },
-            legend: {
-                show: false
-            }
-        });
-        */
 
         $.plot('#pie-chart', data, {
             series: {
@@ -242,9 +267,10 @@ fun.views.dashboard = Backbone.View.extend({
     },
 
     renderRows: function(){
+        // Render rows
         var i = 0;
         var length = this.collection.length;
-        
+
         if (length > 0){
             var rows = this.tbody.html('');
            
@@ -263,8 +289,9 @@ fun.views.dashboard = Backbone.View.extend({
             this.noRecords();
         }
     },
-    
+
     noRecords: function() {
+        // No records
         var template = _.template(
             fun.utils.getTemplate(fun.conf.templates.warning)
         )({message:'noDataAvailable'});
@@ -275,22 +302,27 @@ fun.views.dashboard = Backbone.View.extend({
     },
 
     today: function(event){
+        // Today
         console.log('today event');
     },
 
     thisWeek: function(event) {
+        // This week
         console.log('this week event');
     },
 
     thisMonth: function(event){
+        // This month
         console.log('this month event');
     },
 
     thisYear: function(event){
+        // This year
         console.log('this year event');
     },
 
     setContext: function(event){
+        // Set context org
 
         console.log('setting up activity context');
 
@@ -304,19 +336,10 @@ fun.views.dashboard = Backbone.View.extend({
             }
         });
 
-
-        fun.omnibus.once("some:event", function(){
-          console.log("some event was fired on dashboard!");
-        });
-
-        fun.omnibus.trigger("some:event");
-
         //console.log(fun.conf.hash.dashboard);
         //Backbone.history.loadUrl();
-        //fun.instances.subheader.renderHeadNav();
-        //fun.instances.subheader.noRecords();
-        //return false;
-
+        fun.instances.subheader.renderHeadNav();
+        return false;
         //fun.utils.redirect(fun.conf.hash.dashboard);
     }
 
