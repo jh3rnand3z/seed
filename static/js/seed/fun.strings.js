43aaaf121e18ef57f5c8403550f0c4fc5cb9e747
Jean Chassoul <chassoul@gmail.com>
2015-03-23 19:58:34 -0600

    move move move

--- static/js/seed/fun.strings.js
+++ static/js/seed/fun.strings.js
@@ -35,8 +35,6 @@ fun.strings = {
     seven: "7",
     eight: "8",
     nine: "9",
-    cube: "Cube",
-    cubes: "Cubes",
     oldManAristotle: 'Greek philosopher Aristotle, 320 BC.',
     anAssistant: 'an artificial intelligence and research assistant meant to aid on the creation of artificial intelligence technology systems.',
     ioFunBanner: "Fantastical unbelievable communication.",
